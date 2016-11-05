d620d1fa3df07f9bd1c15040b86e6939e4a47f95
alanyeh20001 <alanyeh20001@hotmail.com>
2016-11-05 15:27:28 +0800

    add journeys route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root "journeys#index"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  resources :journeys
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
