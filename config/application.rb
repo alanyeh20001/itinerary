4099cbc969f82cd4dc6cd66390e257c98edda306
alanyeh20001 <alanyeh20001@hotmail.com>
2016-11-05 14:35:50 +0800

    prevent generator create assets automatically

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module Journey
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Do not create stylesheets, javascript, helper automatically.
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
