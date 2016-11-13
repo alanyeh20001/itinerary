956cb35968b767960d6551684bbca3863f0a052e
alanyeh20001 <alanyeh20001@hotmail.com>
2016-11-14 01:17:08 +0800

    fix sticky menu

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -4,7 +4,8 @@ $(function() {
       menuTop = menu.offset().top,
       titleBarTop = titleBar.offset().top,
       stickyMenuCSS = { "position": "fixed", "top": 0, "width": "100%", "z-index": "99" },
-      stickyTitleBarCSS = { "position": "fixed", "top": "0px", "width": "100%", "z-index": "99" };
+      stickyTitleBarCSS = { "position": "fixed", "top": "0px", "width": "100%", "z-index": "99" },
+      originCSS = { "position": "static" };
 
   window.stickyMenu = function() {
     var scrollTop = $(document).scrollTop();
@@ -12,11 +13,16 @@ $(function() {
     if (menu.css("display") == "block") {
       if (scrollTop > menuTop) {
         menu.css(stickyMenuCSS);
+      } else {
+        menu.css(originCSS);
       }
     } else {
       if (scrollTop > titleBarTop) {
         titleBar.css(stickyTitleBarCSS);
         menu.css({ "position": "fixed", "top": "40px", "z-index": "99" });
+      } else {
+        titleBar.css(originCSS);
+        menu.css(originCSS);
       }
     }
   }
