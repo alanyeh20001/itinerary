28e2b66ddf50a76e69712bab3a3218f7f40e2bdd
alanyeh20001 <alanyeh20001@hotmail.com>
2016-11-25 00:13:41 +0800

    modify fb comment & image size

--- app/models/story.rb
+++ app/models/story.rb
@@ -8,8 +8,8 @@ class Story < ActiveRecord::Base
   has_attached_file :photo,
     path: get_asset_path(DEFAULT_PATH),
     url: get_asset_url(DEFAULT_PATH),
-    styles: { medium: "450x300>" },
-    convert_options: { medium: "-quality 95 -strip" }
+    styles: { medium: "480x320>" },
+    convert_options: { medium: "-quality 100 -strip" }
 
   validates_attachment_presence :photo
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
